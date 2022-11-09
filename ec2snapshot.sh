4f9cb1367b5fc270e8163c72ce1082dbff0b5826
AWS ParallelCluster user <ec2-user@ip-10-0-9-24.ap-southeast-1.compute.internal>
2022-11-09 04:57:35 +0000

    made all files executable

--- ec2snapshot.sh
+++ ec2snapshot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ssh -o StrictHostKeyChecking=no -i /data/MyKeyPair.pem ec2-user@3.0.209.157 "source ~/.bash_profile; pcluster update-compute-fleet --status STOP_REQUESTED --region ap-southeast-1 --cluster-name MyCluster01; ~/update_snapshot.sh data 2 ClusterF"
+ssh -o StrictHostKeyChecking=no -i /data/MyKeyPair.pem ec2-user@3.0.209.157 "source ~/.bash_profile; pcluster update-compute-fleet --status STOP_REQUESTED --region ap-southeast-1 --cluster-name ClusTer; ~/update_snapshot.sh data 2 ClusTer"
--- fsall-slurm.sh
+++ fsall-slurm.sh
@@ -13,7 +13,9 @@
 #SBATCH -e fsall-slurm.%N.%j.err # STDERR
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
-python -u -c "import DataProcessingTools as DPT; \
+python -u -c "import PyHipp as pyh; \
+import time; \
+import DataProcessingTools as DPT; \
 lfall = DPT.objects.processDirs(dirs=None, exclude=['*eye*', '*mountains*'], objtype=pyh.FreqSpectrum, saveLevel=1); \
 lfall.save(); \
 hfall = DPT.objects.processDirs(dirs=None, exclude=['*eye*', '*mountains*'], objtype=pyh.FreqSpectrum, loadHighPass=True, pointsPerWindow=3000, saveLevel=1); \
